0c6acdbd14515e9d894f9a54e9a800a29262389a
amalkrishnaan <amalkrishnaan@gmail.com>
2024-11-29 13:01:22 +0530

    file added

---
+++ server-side/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import env from "dotenv"
+import Connection from "./connection.js"
+import router from "./router.js"
+import cors from 'cors'
+import path from "path"
+
+env.config()
+const app=express()
+app.use(cors())
+app.use(express.static("../client-side/dist"))
+app.use(express.json({limit:"50mb"}))
+app.use('/api',router)
+app.get('/*',(req,res)=>{res.sendFile(path.resolve("../client-side/dist/index.html"))})
+
+Connection().then(()=>{
+    console.log("Database Connected");
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server at http://localhost:${process.env.PORT}`);
+        
+    })
+})
+
+
+
+
+
---
+++ server-side/router.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import * as rh from "./requestHandler.js"
+import Auth from "./middleware/auth.js";
+
+
+const router = Router()
+
+router.route('/getuser').get(Auth,rh.getUser)
+router.route('/getuserdata').get(Auth,rh.getUserData)
+router.route('/edituserdata').post(Auth,rh.editUserData)
+router.route('/deleteuser/:_id').delete(rh.deleteData)
+
+router.route('/addpost').post(Auth,rh.addPost)
+router.route('/getpost').get(Auth,rh.getPost)
+
+
+
+router.route('/signup').post(rh.signUp)
+router.route("/signin").post(rh.signIn)
+router.route("/checkemail").post(rh.checkEmail)
+
+
+
+export default router;
